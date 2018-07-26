598f4ed383cb5e30cf41313ecbf1a987d5a3dd22
EC2 Default User <ec2-user@ip-172-31-16-130.us-east-2.compute.internal>
2018-07-26 14:33:02 +0000

    sql関連の準備

---
+++ page.sql
@@ -0,0 +1,6 @@
+create table page(
+    page_id integer auto_increment,
+    PRIMARY KEY (page_id),
+    title char(200),
+    reration_id integer
+);
---
+++ relation.sql
@@ -0,0 +1,6 @@
+create table relation(
+    reration_id integer auto_increment,
+    PRIMARY KEY (reration_id),
+    code_link TEXT,
+    memo TEXT
+);
