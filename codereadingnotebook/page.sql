2163cf9769e09e78211e7196bb92ea220e254949
keikosanjo <stt.0445@gmail.com>
2018-11-06 15:13:37 +0000

    DB構成変更

--- codereadingnotebook/page.sql
+++ codereadingnotebook/page.sql
@@ -2,8 +2,8 @@ create table page(
     page_id integer auto_increment,
     PRIMARY KEY (page_id),
     title char(200),
-    relation_id integer,
-    FOREIGN KEY (relation_id) REFERENCES relation (relation_id),
+    belong_id integer,
+    FOREIGN KEY (belong_id) REFERENCES belong (belong_id),
     created_at datetime,
     updated_at datetime
 );
--- codereadingnotebook/relation.sql
+++ codereadingnotebook/relation.sql
@@ -2,7 +2,5 @@ create table relation(
     relation_id integer auto_increment,
     PRIMARY KEY (relation_id),
     code_link TEXT,
-    memo TEXT,
-    created_at datetime,
-    updated_at datetime
+    memo TEXT
 );
