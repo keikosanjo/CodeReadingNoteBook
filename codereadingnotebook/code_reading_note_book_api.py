b5622c56c781027bfa3803d2c5aabfb1948f9afb
keikosanjo <stt.0445@gmail.com>
2019-01-08 12:52:22 +0000

    API完成

--- codereadingnotebook/code_reading_note_book_api.py
+++ codereadingnotebook/code_reading_note_book_api.py
@@ -128,12 +128,90 @@ def delete_belong(belong_id):
 def put_belong(belong_id):
     belong_repository = BelongRepository(access_point)
     belong = belong_repository.get(belong_id)
-    for i in numbers:
-        if request.values.get('relation_order_%s'%i) is not None:
-            globals()["relation_order_" + str(i)] = request.values.get('relation_order_%s'%i)
-        else:
-            globals()["relation_order_" + str(i)] = "belong.relation_order_%s"%i
-    relation_order_20 = request.values.get('relation_order_20', None)
+    if request.values.get('relation_order_00') is not None:
+        relation_order_00 = request.values.get('relation_order_00')
+    else:
+        relation_order_00 = belong.relation_order_00
+    if request.values.get('relation_order_01') is not None:
+        relation_order_01 = request.values.get('relation_order_01')
+    else:
+        relation_order_01 = belong.relation_order_01
+    if request.values.get('relation_order_02') is not None:
+        relation_order_02 = request.values.get('relation_order_02')
+    else:
+        relation_order_02 = belong.relation_order_02
+    if request.values.get('relation_order_03') is not None:
+        relation_order_03 = request.values.get('relation_order_03')
+    else:
+        relation_order_03 = belong.relation_order_03
+    if request.values.get('relation_order_04') is not None:
+        relation_order_04 = request.values.get('relation_order_04')
+    else:
+        relation_order_04 = belong.relation_order_04
+    if request.values.get('relation_order_05') is not None:
+        relation_order_05 = request.values.get('relation_order_05')
+    else:
+        relation_order_05 = belong.relation_order_05
+    if request.values.get('relation_order_06') is not None:
+        relation_order_06 = request.values.get('relation_order_06')
+    else:
+        relation_order_06 = belong.relation_order_06
+    if request.values.get('relation_order_07') is not None:
+        relation_order_07 = request.values.get('relation_order_07')
+    else:
+        relation_order_07 = belong.relation_order_07
+    if request.values.get('relation_order_08') is not None:
+        relation_order_08 = request.values.get('relation_order_08')
+    else:
+        relation_order_08 = belong.relation_order_08
+    if request.values.get('relation_order_09') is not None:
+        relation_order_09 = request.values.get('relation_order_09')
+    else:
+        relation_order_09 = belong.relation_order_09
+    if request.values.get('relation_order_10') is not None:
+        relation_order_10 = request.values.get('relation_order_10')
+    else:
+        relation_order_10 = belong.relation_order_10
+    if request.values.get('relation_order_11') is not None:
+        relation_order_11 = request.values.get('relation_order_11')
+    else:
+        relation_order_11 = belong.relation_order_11
+    if request.values.get('relation_order_12') is not None:
+        relation_order_12 = request.values.get('relation_order_12')
+    else:
+        relation_order_12 = belong.relation_order_12
+    if request.values.get('relation_order_13') is not None:
+        relation_order_13 = request.values.get('relation_order_13')
+    else:
+        relation_order_13 = belong.relation_order_13
+    if request.values.get('relation_order_14') is not None:
+        relation_order_14 = request.values.get('relation_order_14')
+    else:
+        relation_order_14 = belong.relation_order_14
+    if request.values.get('relation_order_15') is not None:
+        relation_order_15 = request.values.get('relation_order_15')
+    else:
+        relation_order_15 = belong.relation_order_15
+    if request.values.get('relation_order_16') is not None:
+        relation_order_16 = request.values.get('relation_order_16')
+    else:
+        relation_order_16 = belong.relation_order_16
+    if request.values.get('relation_order_17') is not None:
+        relation_order_17 = request.values.get('relation_order_17')
+    else:
+        relation_order_17 = belong.relation_order_17
+    if request.values.get('relation_order_18') is not None:
+        relation_order_18 = request.values.get('relation_order_18')
+    else:
+        relation_order_18 = belong.relation_order_18
+    if request.values.get('relation_order_19') is not None:
+        relation_order_19 = request.values.get('relation_order_19')
+    else:
+        relation_order_19 = belong.relation_order_19
+    if request.values.get('relation_order_20') is not None:
+        relation_order_20 = request.values.get('relation_order_20')
+    else:
+        relation_order_20 = belong.relation_order_20
     belong = belong_repository.put(belong_id, relation_order_00, relation_order_01, relation_order_02, relation_order_03, relation_order_04, relation_order_05, relation_order_06, relation_order_07, relation_order_08, relation_order_09, relation_order_10, relation_order_11, relation_order_12, relation_order_13, relation_order_14, relation_order_15, relation_order_16, relation_order_17, relation_order_18, relation_order_19, relation_order_20)
     return request.data
 
